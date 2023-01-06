996bbb04e69c68d7df57c67b08694ece0079213e
Diego Lara <89003472+djangohermoso@users.noreply.github.com>
2023-01-06 13:24:19 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -89,7 +89,7 @@ const Navbar = () => {
             </li>
 
             <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
-              <a className="flex justify-between items-center w-full text-gray-300" href="https://drive.google.com/file/d/1RNEoZWZoky3njhGNj4EV8QvYlsHU4SBQ/view?usp=sharing" target="_blank">Resume <BsFillPersonLinesFill size={30}/> </a>
+              <a className="flex justify-between items-center w-full text-gray-300" href="https://drive.google.com/file/d/1U3sIcKPY4UxDRx8PwnZqhAi6WZG8cAoI/view?usp=share_link" target="_blank">Resume <BsFillPersonLinesFill size={30}/> </a>
             </li>
           </ul>
       </div>
